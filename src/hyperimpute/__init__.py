7687fd38697b0a432cf0cc61b6b265e2723a5e04
Bogdan Cebere <bogdan.cebere@gmail.com>
2021-12-30 18:55:37 +0200

    add BO tutorial

--- src/hyperimpute/__init__.py
+++ src/hyperimpute/__init__.py
@@ -1,7 +1,6 @@
 # stdlib
 import os
 import sys
-import warnings
 
 # third party
 import optuna
@@ -10,17 +9,15 @@ import torch
 # hyperimpute relative
 from . import logger  # noqa: F401
 
+optuna.logging.set_verbosity(optuna.logging.FATAL)
+
 optuna.logging.disable_propagation()
 optuna.logging.disable_default_handler()  # Stop showing logs in sys.stderr.
+
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 logger.add(sink=sys.stderr, level="CRITICAL")
-
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-warnings.filterwarnings(
-    "ignore", category=optuna.exceptions.ExperimentalWarning, module="optuna"
-)
 
 os.environ["OMP_NUM_THREADS"] = "2"
 os.environ["OPENBLAS_NUM_THREADS"] = "2"
