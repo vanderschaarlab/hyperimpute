f2ef53c5395c9b4437af37b4fa87e6f2ddf1f907
Bogdan Cebere <bogdan.cebere@gmail.com>
2022-11-06 13:09:50 +0200

    add kneighb algorithms

--- src/hyperimpute/plugins/prediction/regression/plugin_kneighbors_regressor.py
+++ src/hyperimpute/plugins/prediction/regression/plugin_kneighbors_regressor.py
@@ -3,15 +3,15 @@ from typing import Any, List, Optional
 
 # third party
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
 
 # hyperimpute absolute
 import hyperimpute.plugins.core.params as params
-import hyperimpute.plugins.prediction.classifiers.base as base
+import hyperimpute.plugins.prediction.regression.base as base
 
 
-class KNeighborsClassifierPlugin(base.ClassifierPlugin):
-    """Classification plugin based on the KNeighborsClassifier classifier.
+class KNeighborsRegressorPlugin(base.RegressionPlugin):
+    """Classification plugin based on the KNeighborsRegressor classifier.
 
     Example:
         >>> from hyperimpute.plugins.prediction import Predictions
@@ -41,10 +41,10 @@ class KNeighborsClassifierPlugin(base.ClassifierPlugin):
             self.model = model
             return
 
-        self.model = KNeighborsClassifier(
+        self.model = KNeighborsRegressor(
             n_neighbors=n_neighbors,
-            algorithm=KNeighborsClassifierPlugin.algorithm[algorithm],
-            weights=KNeighborsClassifierPlugin.weights[weights],
+            algorithm=KNeighborsRegressorPlugin.algorithm[algorithm],
+            weights=KNeighborsRegressorPlugin.weights[weights],
             leaf_size=leaf_size,
             p=p,
             n_jobs=-1,
@@ -52,15 +52,15 @@ class KNeighborsClassifierPlugin(base.ClassifierPlugin):
 
     @staticmethod
     def name() -> str:
-        return "kneighbors"
+        return "kneighbors_regressor"
 
     @staticmethod
     def hyperparameter_space(*args: Any, **kwargs: Any) -> List[params.Params]:
         return [
             params.Integer(
-                "algorithm", 0, len(KNeighborsClassifierPlugin.algorithm) - 1
+                "algorithm", 0, len(KNeighborsRegressorPlugin.algorithm) - 1
             ),
-            params.Integer("weights", 0, len(KNeighborsClassifierPlugin.weights) - 1),
+            params.Integer("weights", 0, len(KNeighborsRegressorPlugin.weights) - 1),
             params.Integer("n_neighbors", 5, 20),
             params.Integer("leaf_size", 5, 50),
             params.Integer("p", 1, 2),
@@ -68,17 +68,12 @@ class KNeighborsClassifierPlugin(base.ClassifierPlugin):
 
     def _fit(
         self, X: pd.DataFrame, *args: Any, **kwargs: Any
-    ) -> "KNeighborsClassifierPlugin":
+    ) -> "KNeighborsRegressorPlugin":
         self.model.fit(X, *args, **kwargs)
         return self
 
     def _predict(self, X: pd.DataFrame, *args: Any, **kwargs: Any) -> pd.DataFrame:
         return self.model.predict(X, *args, **kwargs)
 
-    def _predict_proba(
-        self, X: pd.DataFrame, *args: Any, **kwargs: Any
-    ) -> pd.DataFrame:
-        return self.model.predict_proba(X, *args, **kwargs)
 
-
-plugin = KNeighborsClassifierPlugin
+plugin = KNeighborsRegressorPlugin
