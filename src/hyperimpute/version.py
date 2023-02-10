45d2541f229adaa2778cc9a303c2d173cd17e04f
Bogdan Cebere <bogdan.cebere@gmail.com>
2023-02-10 15:46:54 +0000

    samplign bugfixing

--- src/hyperimpute/version.py
+++ src/hyperimpute/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 
 MAJOR_VERSION = ".".join(__version__.split(".")[:-1])
 MINOR_VERSION = __version__.split(".")[-1]
--- tests/imputers/test_miracle.py
+++ tests/imputers/test_miracle.py
@@ -1,7 +1,12 @@
+# stdlib
+from typing import Any
+
 # third party
 import numpy as np
+import optuna
 import pandas as pd
 import pytest
+from sklearn.datasets import load_iris
 
 # hyperimpute absolute
 from hyperimpute.plugins.imputers import ImputerPlugin, Imputers
@@ -50,3 +55,30 @@ def test_mean_plugin_fit_transform(test_plugin: ImputerPlugin) -> None:
     )
 
     assert res.isnull().values.any() == False  # noqa
+
+
+def test_param_search() -> None:
+    if len(plugin.hyperparameter_space()) == 0:
+        return
+
+    X, _ = load_iris(return_X_y=True)
+    orig_val = X[0, 0]
+    X[0, 0] = np.nan
+
+    def evaluate_args(**kwargs: Any) -> float:
+        X_imp = plugin(**kwargs).fit_transform(X.copy()).values
+
+        return np.abs(orig_val - X_imp[0, 0])
+
+    def objective(trial: optuna.Trial) -> float:
+        args = plugin.sample_hyperparameters(trial)
+        return evaluate_args(**args)
+
+    study = optuna.create_study(
+        load_if_exists=True,
+        directions=["minimize"],
+        study_name=f"test_param_search_{plugin.name()}",
+    )
+    study.optimize(objective, n_trials=10, timeout=60)
+
+    assert len(study.trials) > 0
